da7ae57b0ac23eef370c928ef4167ee0b3b49ed4
Aayush1222 <ayushshrestha122@gmail.com>
2022-07-06 18:39:02 +0545

    commit on 'main'

--- gui.py
+++ gui.py
@@ -13,3 +13,7 @@ entryfont = ('Garamond', 12)
 # Connecting to the Database where all information will be stored
 connector = sqlite3.connect('SchoolManagement.db')
 cursor = connector.cursor()
+
+connector.execute(
+"CREATE TABLE IF NOT EXISTS SCHOOL_MANAGEMENT (STUDENT_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NAME TEXT, EMAIL TEXT, PHONE_NO TEXT, GENDER TEXT, DOB TEXT, STREAM TEXT)"
+)
